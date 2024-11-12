2f0b659572cef2c28b8e51254f5a16273b67ab77
Giovani Silva <giovani-silva1@live.com>
2024-11-12 20:34:09 -0300

    create GameController, GameRepository and game GameService and create Endpoint FindAll GameMinDTO

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_game")
+@Table(name="tb_game")
 public class Game {
 	
 	@Id
@@ -21,10 +21,14 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String plataforms;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	
@@ -40,7 +44,7 @@ public class Game {
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataforms = plataforms;
+		this.platforms = plataforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -89,12 +93,12 @@ public class Game {
 
 
 	public String getPlataforms() {
-		return plataforms;
+		return platforms;
 	}
 
 
 	public void setPlataforms(String plataforms) {
-		this.plataforms = plataforms;
+		this.platforms = plataforms;
 	}
 
 
